cf42c4e100d8a65aad539e152af295211a351e5a
Wenji Tong <wjtong@gmail.com>
2023-02-03 15:08:22 +0800

    added amqp

--- src/main/java/com/banfftech/camel/Routes.java
+++ src/main/java/com/banfftech/camel/Routes.java
@@ -1,8 +1,11 @@
 package com.banfftech.camel;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.amqp.AMQPComponent;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.apache.camel.support.builder.ExpressionBuilder;
+
+import javax.enterprise.inject.Produces;
 
 public class Routes extends RouteBuilder {
     @Override
@@ -16,6 +19,7 @@ public class Routes extends RouteBuilder {
 
         rest("/parties")
                 .post()
+                .to("amqp:queue:camelQueue")
                 .to("direct:getParties");
 
         from("direct:getParties")
@@ -24,5 +28,11 @@ public class Routes extends RouteBuilder {
                 .process(new OdataProcessor())
 //                .to("olingo4://read/Parties?serviceUri=http://gbms.gbms2.banff-tech.com/odata/control/odataAppSvc/demo");
                 .to("olingo4://read/Parties");
+    }
+
+    @Produces
+    public AMQPComponent getAmqpComponent() {
+        AMQPComponent authorizedAmqp = AMQPComponent.amqpComponent("amqp://localhost:5672", "admin", "ofbiz");
+        return authorizedAmqp;
     }
 }
